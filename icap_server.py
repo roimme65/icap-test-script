e7835ebc42d6da4366e8af5b152a5a7495d6a3cd
roimme65 <roimme@mailbox.org>
2026-02-08 13:43:08 +0100

    Release v1.1.4

--- icap_server.py
+++ icap_server.py
@@ -4,7 +4,7 @@ Simple ICAP Server with ClamAV integration
 Handles REQMOD/RESPMOD requests and scans files with ClamAV
 """
 
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 __author__ = "Roland Imme"
 
 import socket
